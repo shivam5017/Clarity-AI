583501fe0d936aedfaf03bc7e06c19e9dd1b081b
Mac <mac@Macs-MacBook-Pro.local>
2024-11-04 22:34:12 +0530

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Poppins } from "next/font/google";
-import { AuthProvider } from "@/context/AuthContext";
+import { AuthProvider } from "../context/AuthContext";
 
 const poppins = Poppins({
   subsets: ["latin"],
