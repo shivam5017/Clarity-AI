1d58651659d5fda760544d37795d38dea997eae3
Mac <mac@Macs-MacBook-Pro.local>
2024-11-06 22:10:27 +0530

    fix

--- src/app/dashboard/dashboard/page.jsx
+++ src/app/dashboard/dashboard/page.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useState } from "react";
+import React, { useContext} from "react";
 import MyAreaChart from "@/app/aceternity/ui/charts";
 import { AuthContext } from "../../context/AuthContext";
 import Spinner from "@/app/aceternity/spinner";
